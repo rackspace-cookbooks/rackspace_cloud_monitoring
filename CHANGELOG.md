4faadb4b390baa3018cc8584a3cfdcecc876915f
Julien Berard <jujugrrr@gmail.com>
2015-02-20 13:14:44 +0000

    Bumped version (and fix typo in version)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 rackspace_cloud_monitoring CHANGELOG
 ==================
 
-0.4.0
+0.0.5
+-----
+- Fixed behavior on auto-target without alarm #10
+
+0.0.4
 -----
 - No default alarm_criteria for agent.disk, disable alarm if automatic target detection and no alarm_criteria
--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email 'rackspace-cookbooks@rackspace.com'
 license 'Apache 2.0'
 description 'Installs/Configures rackspace_cloud_monitoring'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.4'
+version '0.0.5'
 
 depends 'apt'
 depends 'yum'
